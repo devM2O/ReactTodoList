f0b84b42a1cdc41ade71fe8ab8eee1cf499684bd
minnmawoo <minmawoo.ucsm@gmail.com>
2022-04-02 15:53:27 +0630

    stater file

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import {addTodos} from "../redux/reducer"
 
 const mapStateToProps = (state)=>{
     return {todos: state,}
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return{
+        addTodo: (obj)=>dispatch(addTodos(obj))
+    }
 }
 
 const Todos = (props) => {
@@ -15,8 +22,12 @@ console.log("todo text from store",props)
 
   return <div className="addTodos">
      <input type="text" onChange={(e)=>handleChange(e)} className="todo-input" />
-     <button className="add-btn">Add</button>
+     <button className="add-btn" onClick={()=>props.addTodo({
+        id: Math.floor(Math.random()*1000),
+        item: todo,
+        completed: false,
+     })}>Add</button>
   </div>;
 };
 
-export default connect(mapStateToProps, null)(Todos);
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
